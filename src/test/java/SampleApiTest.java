c3c6d1580ffa47cffc50e8aaf309a90974c63c30
Ismayatikiran <ismaytaikiran96@gmail.com>
2020-09-12 16:36:03 -0400

    RAPI4

--- src/test/java/SampleApiTest.java
+++ src/test/java/SampleApiTest.java
@@ -3,7 +3,6 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,7 +10,7 @@ import java.nio.file.Paths;
 
 public class SampleApiTest {
 
-    public static void dologin() throws IOException {
+    public static String dologin() throws IOException {
         String url = "http://localhost:8090/rest/auth/1/session";
         String loginPayload = new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "/payloads/login.json")));
 
@@ -24,10 +23,16 @@ public class SampleApiTest {
 
         String stringResponse = response.asString();
         JsonPath jsonPath = new JsonPath(stringResponse);
+
+        //String has been created to get the SessionID
         String sessionId = jsonPath.get("session.value");
         System.out.println("Session Id: "+sessionId);
+
         String loginPrevioustime = jsonPath.get("loginInfo.previousLoginTime");
         System.out.println("Login previous time: "+loginPrevioustime);
+
+        return sessionId;
+
     }
 
 
@@ -35,14 +40,26 @@ public class SampleApiTest {
 
 
     public static void creatIssueTest() throws IOException {
-        String url = "http://localhost:8090/rest/auth/2/session";
-        String creatIssuePayload = new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+"/payloads/createIssue.json")));
-        RequestSpecification requestSpecification = RestAssured.given().body(creatIssuePayload);
-        requestSpecification.contentType(ContentType.JSON);
-        requestSpecification.cookie("");
+        String url = "http://localhost:8090/rest/api/2/issue";
+        String createIssuePayload = new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+"/payloads/createIssue.json")));
+        RequestSpecification requestSpecification = RestAssured.given().body(createIssuePayload);
+
+        //Specify headers
+         requestSpecification.contentType(ContentType.JSON);
+        // requestSpecification.cookie("JSESSIONID=E8B3D1577B4505CA45FDB22D622EFB4F");
 
 
-      //  dologin();
+
+         String sessionID= "JSESSIONID="+dologin();
+         requestSpecification.header("Cookie",sessionID);
+
+
+        //Making a call
+        Response response = requestSpecification.post(url);
+        System.out.println("Status Code: "+response.statusCode());
+
+
+
     }
 
 
@@ -52,6 +69,7 @@ public class SampleApiTest {
 
 
     public static void main(String[] args) throws IOException {
-        creatIssueTest();
+       creatIssueTest();
+       // dologin();
     }
 }
