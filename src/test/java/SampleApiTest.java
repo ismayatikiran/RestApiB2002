4234fc09cee7e079189ae550d72b28d85a5b3631
Ismayatikiran <ismaytaikiran96@gmail.com>
2020-09-12 17:25:43 -0400

    RAPI4.1

--- src/test/java/SampleApiTest.java
+++ src/test/java/SampleApiTest.java
@@ -3,12 +3,15 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class SampleApiTest {
+
 
     public static String dologin() throws IOException {
         String url = "http://localhost:8090/rest/auth/1/session";
@@ -17,7 +20,8 @@ public class SampleApiTest {
         RequestSpecification requestSpecification = new RestAssured().given().body(loginPayload);
         requestSpecification.contentType(ContentType.JSON);
         Response response = requestSpecification.post(url);
-//        System.out.println(response);
+        //System.out.println(response);
+
        System.out.println("Status Code: "+response.getStatusCode()+"\n"
              +response.asString());
 
@@ -28,6 +32,7 @@ public class SampleApiTest {
         String sessionId = jsonPath.get("session.value");
         System.out.println("Session Id: "+sessionId);
 
+        //Printing Json Path
         String loginPrevioustime = jsonPath.get("loginInfo.previousLoginTime");
         System.out.println("Login previous time: "+loginPrevioustime);
 
@@ -39,37 +44,37 @@ public class SampleApiTest {
 
 
 
-    public static void creatIssueTest() throws IOException {
+    public static Response createIssue() throws IOException {
         String url = "http://localhost:8090/rest/api/2/issue";
         String createIssuePayload = new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+"/payloads/createIssue.json")));
         RequestSpecification requestSpecification = RestAssured.given().body(createIssuePayload);
 
         //Specify headers
          requestSpecification.contentType(ContentType.JSON);
-        // requestSpecification.cookie("JSESSIONID=E8B3D1577B4505CA45FDB22D622EFB4F");
 
-
-
+        //Setting the SessionID by passing through login
          String sessionID= "JSESSIONID="+dologin();
          requestSpecification.header("Cookie",sessionID);
-
 
         //Making a call
         Response response = requestSpecification.post(url);
         System.out.println("Status Code: "+response.statusCode());
 
-
+        return response;
 
     }
 
+@Test
+public static void createIssueTest() throws IOException {
+        int statusCode = createIssue().statusCode();
+    Assert.assertEquals(201,statusCode);
+    System.out.println("Test Passed with Status code 201: "+statusCode);
 
 
-
-
-
-
-    public static void main(String[] args) throws IOException {
-       creatIssueTest();
-       // dologin();
-    }
 }
+
+
+
+
+
+    }
